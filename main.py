9ced86f87f4fcb51777a8cbbbdea958c833c35c6
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-04-13 12:46:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-#y = 0
-
-#def posunuti():
-#    global y
-#    for i in range(5):
-#        led.unplot(i, y)
-#    y = (y+1)%5
-#    for i in range(5):
-#        led.plot(i, y)
-
-
-#    pause(3000)
-
-#def on_button_pressed_a():
-#    basic.forever(posunuti)
-#input.on_button_pressed(Button.A, on_button_pressed_a)
-
 x = 0
 def posunuti_horizontal():
     global x
